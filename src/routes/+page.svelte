a0854ba42b551b4b5c7dcedb270d0671a8f9c1f7
choudel <chouaibh@gmail.com>
2022-09-24 02:31:00 +0200

    redux fondamentals

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+import store from "../store";
+import {bugAdded} from "../actions"
+const unsubscribe = store.subscribe(()=>{
+    console.log("Store changed!",store.getState())
+})
+
+store.dispatch(bugAdded("Bug 1"))
+unsubscribe();
+
+</script>
